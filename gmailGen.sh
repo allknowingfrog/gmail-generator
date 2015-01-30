eb4e8be7755a818a00550bc953ec97998dce3994
allknowingfrog <allknowingfrog@gmail.com>
2015-01-30 13:00:16 -0600

    assign default value without if statement

--- gmailGen.sh
+++ gmailGen.sh
@@ -4,11 +4,7 @@ if [ -z "$1" ]; then
     exit
 fi
 EMAIL=$1
-if [ -z "$2" ]; then
-    TOTAL=10
-else
-    TOTAL=$2
-fi
+TOTAL=${2:-10}
 COUNTER=0
 while [ $COUNTER -lt $TOTAL ]; do
     echo ${EMAIL/@/+$COUNTER@}
